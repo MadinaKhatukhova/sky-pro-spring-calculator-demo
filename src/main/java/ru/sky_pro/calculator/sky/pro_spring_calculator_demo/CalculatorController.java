d3e182e0ae42d7cc90021c3e22d3474513cafda8
madinakhatukhova <Madina-Khat@yandex.ru>
2025-01-28 14:16:59 +0300

    Проверка Maven

---
+++ src/main/java/ru/sky_pro/calculator/sky/pro_spring_calculator_demo/CalculatorController.java
@@ -0,0 +1,48 @@
+package ru.sky_pro.calculator.sky.pro_spring_calculator_demo;
+
+public class CalculatorController {
+
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+
+    public String showGreetings() {
+        return "<h1>Добро пожаловать в калькулятор.</h1>";
+    }
+
+
+    public String sumNumbers( Integer num1,  Integer num2) {
+        Integer result = calculatorService.sum(num1, num2);
+        return generateMessage(num1, num2, '+', result);
+    }
+
+
+    public String subtractNumbers( Integer num1,  Integer num2) {
+        int result = calculatorService.subtract(num1, num2);
+        return generateMessage(num1, num2, '-', result);
+    }
+
+
+    public String multiplyNumbers( Integer num1,  Integer num2) {
+        int result = calculatorService.multiply(num1, num2);
+        return generateMessage(num1, num2, '*', result);
+    }
+
+
+    public String divideNumbers( Integer num1, Integer num2) {
+        if (num2 == 0) {
+
+            throw new IllegalArgumentException("mistake");
+        }
+
+        int result = calculatorService.divide(num1, num2);
+        return generateMessage(num1, num2, '/', result);
+    }
+
+    private String generateMessage(Integer num1, Integer num2, char action, int result) {
+        return String.format("<h1>%d %c %d = %d</h1>", num1, action, num2, result);
+    }
+}
