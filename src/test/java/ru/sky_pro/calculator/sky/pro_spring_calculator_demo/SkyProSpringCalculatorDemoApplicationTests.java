5fa4a502ddd0d5fc6e90302e9362e48cc23f2d14
madinakhatukhova <Madina-Khat@yandex.ru>
2025-01-28 13:42:27 +0300

    Initial commit

---
+++ src/test/java/ru/sky_pro/calculator/sky/pro_spring_calculator_demo/SkyProSpringCalculatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sky_pro.calculator.sky.pro_spring_calculator_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProSpringCalculatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
