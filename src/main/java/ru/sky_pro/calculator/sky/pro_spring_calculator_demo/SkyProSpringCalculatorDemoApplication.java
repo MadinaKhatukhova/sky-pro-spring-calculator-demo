e41fdfd3318ba7b899f58e89c16f2c0ec2fec459
madinakhatukhova <Madina-Khat@yandex.ru>
2025-01-28 13:42:27 +0300

    Ошибка

---
+++ src/main/java/ru/sky_pro/calculator/sky/pro_spring_calculator_demo/SkyProSpringCalculatorDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.sky_pro.calculator.sky.pro_spring_calculator_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProSpringCalculatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyProSpringCalculatorDemoApplication.class, args);
+	}
+
+}
