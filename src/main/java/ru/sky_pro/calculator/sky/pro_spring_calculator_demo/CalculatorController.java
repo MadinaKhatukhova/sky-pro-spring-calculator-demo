a4364ff0da33ea5df679c6620126889188d17078
madinakhatukhova <Madina-Khat@yandex.ru>
2025-02-03 14:57:09 +0300

    Проверка Maven_исправление

--- src/main/java/ru/sky_pro/calculator/sky/pro_spring_calculator_demo/CalculatorController.java
+++ src/main/java/ru/sky_pro/calculator/sky/pro_spring_calculator_demo/CalculatorController.java
@@ -1,5 +1,11 @@
 package ru.sky_pro.calculator.sky.pro_spring_calculator_demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
 
     private final CalculatorService calculatorService;
@@ -8,30 +14,29 @@ public class CalculatorController {
         this.calculatorService = calculatorService;
     }
 
-
+    @GetMapping
     public String showGreetings() {
         return "<h1>Добро пожаловать в калькулятор.</h1>";
     }
-
-
+    @GetMapping("/plus")
     public String sumNumbers( Integer num1,  Integer num2) {
         Integer result = calculatorService.sum(num1, num2);
         return generateMessage(num1, num2, '+', result);
     }
 
-
+    @GetMapping("/minus")
     public String subtractNumbers( Integer num1,  Integer num2) {
         int result = calculatorService.subtract(num1, num2);
         return generateMessage(num1, num2, '-', result);
     }
 
-
+    @GetMapping("/multiply")
     public String multiplyNumbers( Integer num1,  Integer num2) {
         int result = calculatorService.multiply(num1, num2);
         return generateMessage(num1, num2, '*', result);
     }
 
-
+    @GetMapping("/divide")
     public String divideNumbers( Integer num1, Integer num2) {
         if (num2 == 0) {
 
