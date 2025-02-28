98da0b7bcf4cab203f19e1aefbf08264b00b8632
madinakhatukhova <Madina-Khat@yandex.ru>
2025-02-28 12:13:00 +0300

    test1

---
+++ src/main/java/ru/sky_pro/calculator/sky/pro_spring_calculator_demo/service/ZeroDivideException.java
@@ -0,0 +1,13 @@
+package ru.sky_pro.calculator.sky.pro_spring_calculator_demo.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+
+public class ZeroDivideException extends Throwable {
+
+    public ZeroDivideException(){
+        super("Ошибка деление на ноль");
+    }
+}
---
+++ src/test/java/ru/sky_pro/calculator/sky/pro_spring_calculator_demo/CalculatorServiceImplParameterTest.java
@@ -0,0 +1,43 @@
+package ru.sky_pro.calculator.sky.pro_spring_calculator_demo;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static java.lang.System.out;
+import static ru.sky_pro.calculator.sky.pro_spring_calculator_demo.CalculatorServiceImplTestConstants.*;
+
+public class CalculatorServiceImplParameterTest {
+
+
+    @MethodSource("provideArgumentsForCalculatorTests")
+    @ParameterizedTest
+    public void shouldReturnCorrectResultOfSubtraction(int num1, int num2) {
+        assertEquals(num1 - num2, out.subtract(num1, num2));
+    }
+
+    @MethodSource("provideArgumentsForCalculatorTests")
+    @ParameterizedTest
+    public void shouldReturnCorrectResultOfMultiplication(int num1, int num2) {
+        assertEquals(num1 + num2, out.multiply(num1, num2));
+    }
+
+    @MethodSource("provideArgumentsForCalculatorTests")
+    @ParameterizedTest
+    public void shouldReturnCorrectResultOfDivision(int num1, int num2) {
+        assertEquals(num1 / num2, out.divide(num1, num2));
+    }
+
+    private void assertEquals(int i, Integer divide) {
+
+    }
+
+    private static Stream<Arguments> provideArgumentsForCalculatorTests() {
+        return Stream.of(Arguments.of(ZERO, ONE), Arguments.of(ZERO, TWO), Arguments.of(ONE, ONE), Arguments.of(ONE, TWO), Arguments.of(TWO, ONE)
+
+        );
+    }
+
+}
